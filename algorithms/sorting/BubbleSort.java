fe231f65d1b50913bdb78b3914136e3b11009325
lauramoroni <moronilaura28@gmail.com>
2025-03-18 20:07:56 -0300

    implement bubble sort algorithm

--- algorithms/sorting/BubbleSort.java
+++ algorithms/sorting/BubbleSort.java
@@ -1,3 +1,15 @@
-public class BubbleSort {
+public class BubbleSort<T> implements Sort<T> {
+   public void sort(T[] array) {
+      int size = array.length;
 
+      for (int i = 0; i < size - 1; i++) {
+         for (int j = 0; j < size - i - 1; j++) {
+            if (((Comparable<T>) array[j]).compareTo(array[j + 1]) > 0) {
+               T temp = array[j];
+               array[j] = array[j + 1];
+               array[j + 1] = temp;
+            }
+         }
+      }
+   }
 }
