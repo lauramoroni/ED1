580a064f7f4c22a1687f745cf610145ad5f37127
lauramoroni <moronilaura28@gmail.com>
2025-03-18 15:55:50 -0300

    search method BST

--- algorithms/data_structures/BinaryTree.java
+++ algorithms/data_structures/BinaryTree.java
@@ -1,3 +1,45 @@
 public class BinaryTree {
+   // classe que representa cada nó na árvore binária
+   class Node {
+      int key;
+      Node left, right, parent;
 
+      Node(int key) {
+         this.key = key;
+         left = null;
+         right = null;
+         parent = null;
+      }
+   }
+
+   // atributos da árvore
+   private Node root;
+   private int size;
+
+   public BinaryTree() {
+      this.root = null;
+      this.size = 0;
+   }
+
+   public int getSize() {
+      return this.size;
+   }
+
+   public boolean isEmpty() {
+      return this.root == null;
+   }
+
+   public Node search(int key) {
+      Node current = this.root;
+      
+      while (current != null) {
+         if (key < current.key) {
+            current = current.left;
+         } else {
+            current = current.right;
+         }
+      }
+
+      return current;
+   }
 }
